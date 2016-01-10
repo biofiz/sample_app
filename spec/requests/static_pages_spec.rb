0690277f41285d5b1ef63ecf14594c2118312a5d
Vladimir Khomich <biofiz@gmail.com>
2016-01-10 10:33:15 +0200

    Test for Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/'
       expect(page).to have_content('Sample App')
-      expect(page).to have_title('Home | Ruby on Rails Tutorial Sample App')
+      expect(page).to have_title("Home | #{base_title}")
     end
   end
 
@@ -16,7 +18,7 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
-      expect(page).to have_title('Help | Ruby on Rails Tutorial Sample App')
+      expect(page).to have_title("Help | #{base_title}")
     end
   end
 
@@ -25,7 +27,16 @@ describe "Static pages" do
     it "should have the content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
-      expect(page).to have_title('About | Ruby on Rails Tutorial Sample App')
+      expect(page).to have_title("About | #{base_title}")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+      expect(page).to have_title("Contact | #{base_title}")
     end
   end
 end
